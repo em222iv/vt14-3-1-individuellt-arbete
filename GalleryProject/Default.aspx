223eacf39767828e41225b5f51ecb22c50a0725b
em222iv <em222iv@student.lnu.se>
2014-03-06 09:25:49 +0100

    projectstart

---
+++ GalleryProject/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.WebForm1" ViewStateMode="Disabled" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <link href="Style.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ValidationSummary ID="ValidationSummary1" runat="server" HeaderText="ValidationSummary: zup. much bugg, very no" />
+    <div>
+        <div id="picture"  >
+            <div id="bigImg">
+            <asp:Image ID="Image" runat="server" Width="550" Height="375" Visible="false" />
+                </div>
+        </div>
+         <div><asp:Label ID="successLabel" runat="server" Text="Uppladdningen lyckades" Visible="false"></asp:Label></div>
+         <div id="thumbnails">
+              <asp:Repeater ID="repeater" runat="server" ItemType="System.String" SelectMethod="repeater_GetData">
+                <ItemTemplate>
+                    <asp:HyperLink ID="FileHyperLink" runat="server"
+                        Text='<%#Item %>'
+                        ImageUrl='<%#"Content/thumbImg/" + Item%>'
+                        NavigateUrl='<%#"?" + Item %>'/>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+        <div id="hejhopp">
+            <div id="fileBrowser"><asp:FileUpload ID="fileBrowse" runat="server" AllowMultiple="True" /></div>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Choose a picture to upload" ControlToValidate="fileBrowse"></asp:RequiredFieldValidator>
+            <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ErrorMessage="*JPG/JPEG/GIF files only" ValidationExpression="^.+\.(([jJ][pP][eE]?[gG])|([gG][iI][fF])|([pP][nN][gG]))$"  ControlToValidate="fileBrowse"></asp:RegularExpressionValidator>
+            <div id="uploadButton"><asp:Button ID="Button" runat="server" Text="uploadPic" OnClick="Button_Click" /></div>
+            <div id="deleteButton"><asp:Button ID="Button1" runat="server" Text="deletePic" OnClick="deleteButton_Click" causesValidation="false"/></div>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ GalleryProject/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private bool IsUploadSuccess
+        {
+            set { Session["UploadSuccess"] = value; }
+            get
+            {
+                var isUploadSuccess = Session["UploadSuccess"] as bool? ?? false;
+                Session.Remove("UploadSuccess");
+                return isUploadSuccess;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var ImageQuery = Request.QueryString;
+            Image.ImageUrl = "Content/Images/" + ImageQuery;
+            Image.Visible = true;
+
+            if (IsUploadSuccess)
+            {
+                successLabel.Visible = true;
+            }
+        }
+
+       /* public IEnumerable<System.String> repeater_GetData()
+        {
+            Gallery Gallery = new Gallery();
+            return Gallery.GetImageName();
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                try
+                {
+                    Gallery Gallery = new Gallery();
+                    var file = fileBrowse.FileContent;
+                    var filename = fileBrowse.FileName;
+                    Gallery.SaveImage(file, filename);
+                    IsUploadSuccess = true;
+                    Response.Redirect("?" + filename);
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.AddModelError(String.Empty, ex.Message);
+                }
+            }
+        }
+
+        protected void deleteButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Gallery Gallery = new Gallery();
+                var ImageQuery = Request.QueryString;
+
+                Gallery.deleteImage(ImageQuery.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError(String.Empty, ex.Message);
+            }
+
+            Image.ImageUrl = "";
+        }
+
+    }
+}*/
